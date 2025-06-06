0acc49ac5ccf86d27409718f1f87f8562bed6615
GustavoMiguelk <miguelgustavo004@gmail.com>
2025-06-06 16:55:18 -0300

    feat: task routes completed

--- src/controller/task-controller.ts
+++ src/controller/task-controller.ts
@@ -50,4 +50,68 @@ export class TaskController {
 
         response.json({ taskCreated })
     }
+
+    async update(request: Request, response: Response){
+        const bodySchema = z.object({
+            id: z.string(),     
+            status: z.enum(["pending", "progressing", "completed"])
+        })
+
+        const { id ,status } = bodySchema.parse(request.body)
+
+        const hasTask = await prisma.tasks.findFirst({ where: { id }})
+
+        if(!hasTask){
+            throw new AppError("This task does not exist")
+        }
+
+        if(hasTask.status === "completed"){
+            throw new AppError("This task is already completed")
+        }
+
+        if(hasTask.assignedTo !== request.user?.id){
+            throw new AppError("Unauthorized")
+        }
+
+        const taskStatus = await prisma.tasks.update({
+            data:{
+                status,
+            },
+            where: {
+                id,
+            }
+        })
+
+        response.json({ taskStatus })
+    }
+
+    async show(request: Request, response: Response){
+        const memberTeam = await prisma.members.findFirst({
+            where: {userId: request.user?.id}
+        })
+
+        const tasks = await prisma.tasks.findMany({
+            where: { teamId: memberTeam?.teamId }
+        })
+
+        response.json({tasks})
+    }
+
+    async remove(request: Request, response: Response){
+        const { id } = request.params
+
+        const hasTask = await prisma.tasks.findFirst({
+            where:{ id }
+        })
+
+        if(!hasTask){
+            throw new AppError("This task does not exist")
+        }
+
+        await prisma.tasks.delete({
+            where: { id }
+        })
+
+        response.json({ message: "Task Deleted" })
+    }
 }
--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express"; 
 import { TaskController } from "@/controller/task-controller";
+import { ensureAuth } from "@/middleware/ensure-auth";
+import { verifyUserAuth } from "@/middleware/verify-auth";
 
 export const taskRoutes = Router()
 const taskController = new TaskController()
 
-taskRoutes.post("/", taskController.create)
+taskRoutes.use(ensureAuth)
+
+taskRoutes.post("/", verifyUserAuth(["admin"]),taskController.create)
+taskRoutes.delete("/:id", verifyUserAuth(["admin"]),taskController.remove)
+
+taskRoutes.patch("/", taskController.update)
+taskRoutes.get("/", taskController.show)
